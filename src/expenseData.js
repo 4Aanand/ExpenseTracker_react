ade8489ca0c0d3d1054598031c274be8ee65ae6a
4Aanand <anand999gkp@gmail.com>
2025-09-13 22:35:58 +0530

    somefix

--- src/expenseData.js
+++ src/expenseData.js
@@ -1,30 +1,9 @@
 export default [
-    // {
-    //     id: crypto.randomUUID(),
-    //     name:'Aanand',
-    //     title: 'Milk',
-    //     category: 'Grocery',
-    //     amount: 40,
-    // },
-    // {
-    //     id: crypto.randomUUID(),
-    //     name:'Aanand',
-    //     title: 'Shirt',
-    //     category: 'Clothes',
-    //     amount: 600,
-    // },
-    // {
-    //     id: crypto.randomUUID(),
-    //     name:'Aanand',
-    //     title: 'Vegetables',
-    //     category: 'Grocery',
-    //     amount: 100,
-    // },
     {
         id: crypto.randomUUID(),
-        name:'',
-        title: '',
-        category: '',
-        amount: '',
+         name: "",
+    title: "",
+    category: "",
+    amount: "",
     },
 ]
